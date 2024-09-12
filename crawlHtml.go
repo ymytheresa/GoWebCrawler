4fe17a33a66af5a2a4f7695e9962da3bbd2f8a67
Theresa <ymy.theresa@gmail.com>
2024-09-12 17:48:23 +0800

    crawl html

--- crawlHtml.go
+++ crawlHtml.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
 func crawlHtml(url string) {
 	fmt.Printf("starting crawl of: %s", url)
+	htmlbody, _ := getHtml(url)
+	getURLsFromHTML(htmlbody, url)
 	os.Exit(0)
+}
+
+func getHtml(url string) (string, error) {
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	resType := res.Header.Get("Content-Type")
+	if res.StatusCode >= 400 {
+		return "", fmt.Errorf("code > 299")
+	} else if res.StatusCode > 200 {
+		return "", fmt.Errorf(res.Status)
+	}
+	if resType == "text" || resType == "html" {
+		return "", fmt.Errorf("not html or text")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
+
+	return string(body), nil
 }
