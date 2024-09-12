40ede3397d98218482c1c5f8e06bbf9cc75383cb
Theresa <ymy.theresa@gmail.com>
2024-09-12 17:49:52 +0800

    crawl html by url

--- crawlHtml.go
+++ crawlHtml.go
@@ -3,39 +3,40 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func crawlHtml(url string) {
 	fmt.Printf("starting crawl of: %s", url)
-	htmlbody, _ := getHtml(url)
+	htmlbody, _ := getHTML(url)
 	getURLsFromHTML(htmlbody, url)
 	os.Exit(0)
 }
 
-func getHtml(url string) (string, error) {
-
-	res, err := http.Get(url)
+func getHTML(rawURL string) (string, error) {
+	res, err := http.Get(rawURL)
 	if err != nil {
-		log.Fatal(err)
+		return "", fmt.Errorf("got Network error: %v", err)
 	}
-	body, err := io.ReadAll(res.Body)
-	res.Body.Close()
-	resType := res.Header.Get("Content-Type")
-	if res.StatusCode >= 400 {
-		return "", fmt.Errorf("code > 299")
-	} else if res.StatusCode > 200 {
-		return "", fmt.Errorf(res.Status)
-	}
-	if resType == "text" || resType == "html" {
-		return "", fmt.Errorf("not html or text")
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s", body)
+	defer res.Body.Close()
 
-	return string(body), nil
+	if res.StatusCode > 399 {
+		return "", fmt.Errorf("got HTTP error: %s", res.Status)
+	}
+
+	contentType := res.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "text/html") {
+		return "", fmt.Errorf("got non-HTML response: %s", contentType)
+	}
+
+	htmlBodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("couldn't read response body: %v", err)
+	}
+
+	htmlBody := string(htmlBodyBytes)
+
+	return htmlBody, nil
 }
